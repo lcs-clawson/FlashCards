0244728806cf9112f30522b4449b390e1e6fa306
Cooper Lawson <CSLawson@lcs.on.ca>
2023-02-07 08:45:29 -0500

    Initial Commit

---
+++ FlashCards/FlashCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashCardsApp.swift
+//  FlashCards
+//
+//  Created by Cooper Lawson on 2023-02-07.
+//
+
+import SwiftUI
+
+@main
+struct FlashCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
