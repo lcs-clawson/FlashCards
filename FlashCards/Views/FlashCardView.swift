67184cfa2e4dddbf632822e99ccc1de12ce83a5f
Cooper Lawson <CSLawson@lcs.on.ca>
2023-02-07 09:11:13 -0500

    Organized the views and deleted ContentView.

---
+++ FlashCards/Views/FlashCardView.swift
@@ -0,0 +1,20 @@
+//
+//  FlashCardView.swift
+//  FlashCards
+//
+//  Created by Cooper Lawson on 2023-02-07.
+//
+
+import SwiftUI
+
+struct FlashCardView: View {
+    var body: some View {
+        Text("FlashCards")
+    }
+}
+
+struct FlashCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FlashCardView()
+    }
+}
